630a1f32716a46afc676f968ff753676c64a5a85
Paul Cwik <paul@cwik.us>
2021-04-08 20:31:15 -0500

    added suggestion feature

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,12 @@
+// reservation form
 let $submitRSVP = document.getElementById('submit-rsvp');
 let $fullName = document.getElementById('full_name');
 let $email = document.getElementById('email');
 let $telephone = document.getElementById('icon_telephone');
 
-
+// suggestion form
+let $submitSugg = document.getElementById('submit-suggestion');
+let $suggestion = document.getElementById('suggestion');
 
 $submitRSVP.addEventListener('click', function(e) {
     e.preventDefault();
@@ -18,5 +21,19 @@ $submitRSVP.addEventListener('click', function(e) {
         $('#register-form').html(
             `<h3 class='center font-lobster'>Thank you for registering.  We'll see you soon!</h3>`
         )
+    })
+})
+
+
+$submitSugg.addEventListener('click', function(e) {
+    e.preventDefault();
+    let userForm = {
+        suggestion: $suggestion.value.trim()
+    }
+    console.log(userForm);
+    $.post('/api/sugg', userForm).then(response => {
+        console.log(response);
+        $suggestion.setAttribute("data-success", "Suggestion Recieved");
+        $suggestion.value = ''
     })
 })
