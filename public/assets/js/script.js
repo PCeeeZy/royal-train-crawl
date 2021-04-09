6e200b2a16daf87b8b02dd30bcbe7919d619a8df
Paul Cwik <paul@cwik.us>
2021-04-09 10:31:14 -0500

    refactored script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,32 +8,59 @@ let $telephone = document.getElementById('icon_telephone');
 let $submitSugg = document.getElementById('submit-suggestion');
 let $suggestion = document.getElementById('suggestion');
 
+// --------------- EVENT HANDLERS ---------------
+// rsvp submission handler
 $submitRSVP.addEventListener('click', function(e) {
     e.preventDefault();
     let userForm = {
         full_name: $fullName.value.trim(),
         email: $email.value.trim(),
         phone: $telephone.value.trim()
-    }
-    console.log(userForm);
-    $.post('/api/rsvp', userForm).then(response => {
-        console.log(response);
-        $('#register-form').html(
-            `<h3 class='center font-lobster'>Thank you for registering.  We'll see you soon!</h3>`
-        )
+    };
+
+    $("#register-form").empty();
+    $('#gonna-spin-register').html(renderSpinner());
+
+    $.post('/api/rsvp', userForm)
+        .then(response => {
+            $('#gonna-spin-register').empty();
+            $('#register-form').html(registerSuccess());
     })
 })
 
-
+// suggestion submission handler
 $submitSugg.addEventListener('click', function(e) {
     e.preventDefault();
     let userForm = {
         suggestion: $suggestion.value.trim()
-    }
-    console.log(userForm);
-    $.post('/api/sugg', userForm).then(response => {
-        console.log(response);
-        $suggestion.setAttribute("data-success", "Suggestion Recieved");
-        $suggestion.value = ''
-    })
-})
+    };
+
+    $('#suggestion').hide();
+    $('#gonna-spin-suggestion').html(renderSpinner());
+
+    $.post('/api/sugg', userForm)
+        .then(response => {
+            $('#gonna-spin-suggestion').empty();
+            $('#suggestion').show();
+            $suggestion.value = '';
+    });
+});
+
+// ---------- utilities -------------------
+function renderSpinner() {
+    return ` <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div><div class="gap-patch">
+        <div class="circle"></div>
+      </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>`;
+};
+
+function registerSuccess() {
+    return  `<h3 class='center font-lobster'>Thank you for registering.  We'll see you soon!</h3>`;
+};
